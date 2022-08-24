c97c916137900b75c9e1e3d1bc5051f7624ee6d0
Sebastian Kloppe <yeahdev@gmail.com>
2022-08-24 08:40:28 +0000

    add check login endpoint

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,13 @@
-const { Client } = require('@notionhq/client');
+const { Client } = require("@notionhq/client");
+const bcrypt = require("bcrypt");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const notion = new Client({ auth: process.env.NOTION_API_TOKEN });
 
 const checkAuth = (headers) => {
-  if ('x-auth-token' in headers) {
-    const authToken = headers['x-auth-token'];
+  if ("x-auth-token" in headers) {
+    const authToken = headers["x-auth-token"];
     return authToken === process.env.AUTH_TOKEN;
   }
 
@@ -14,25 +15,69 @@ const checkAuth = (headers) => {
 };
 
 const getDatabaseActiveId = () => {
-  return process.env.NODE_ENV === 'production' ? process.env.NOTION_DB_ACTIVE_ID : process.env.NOTION_DB_ACTIVE_ID_TEST;
+  return process.env.NODE_ENV === "production"
+    ? process.env.NOTION_DB_ACTIVE_ID
+    : process.env.NOTION_DB_ACTIVE_ID_TEST;
 };
 
 const getDatabaseId = () => {
-  return process.env.NODE_ENV === 'production' ? process.env.NOTION_DB_ID : process.env.NOTION_DB_ID_TEST;
+  return process.env.NODE_ENV === "production"
+    ? process.env.NOTION_DB_ID
+    : process.env.NOTION_DB_ID_TEST;
 };
 
 const createUuidV4 = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (char) => {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (char) => {
     const random = (Math.random() * 16) | 0;
-    const value = char === 'x' ? random : (random % 4) + 8;
+    const value = char === "x" ? random : (random % 4) + 8;
     return value.toString(16);
   });
+};
+
+const checkUser = async (headers) => {
+  const _getUserFromHeader = (headers) => {
+    const authHeader = headers["authorization"];
+    const base64UserCred = authHeader.split(" ")[1];
+    const userCred = Buffer.from(base64UserCred, "base64").toString();
+    const username = userCred.split(":")[0];
+    const pw = userCred.split(":")[1];
+    return { username, pw };
+  };
+
+  const _getAllowedUsers = () => {
+    const splittedUsers = process.env.USERS.split(" ");
+    return splittedUsers
+      .map((user) => user.split(":"))
+      .map((splits) => ({ username: splits[0], pw: splits[1] }));
+  };
+
+  if (!("authorization" in headers)) {
+    return false;
+  }
+
+  const requestedUser = _getUserFromHeader(headers);
+  const allowedUsers = _getAllowedUsers();
+
+  const foundAllowedUser = allowedUsers.find(
+    (allowedUser) => allowedUser.username === requestedUser.username
+  );
+  if (!foundAllowedUser) {
+    return false;
+  }
+
+  const match = await bcrypt.compare(requestedUser.pw, foundAllowedUser.pw);
+  if (!match) {
+    return false;
+  }
+
+  return true;
 };
 
 module.exports = {
   notion,
   checkAuth,
   createUuidV4,
+  checkUser,
   getDatabaseId,
   getDatabaseActiveId,
 };
